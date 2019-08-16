dacb069669f24024a02365f8442779051c50298d
Greg Wilson <gvwilson@third-bit.com>
2019-08-16 05:44:55 -0400

    Removing tbLog and associated buttons and controls.
    Greg was over-engineering things again...

--- generators/js/data_mtcars.js
+++ generators/js/data_mtcars.js
@@ -5,6 +5,5 @@ Blockly.JavaScript['data_mtcars'] = (block) => {
   const URL = 'https://raw.githubusercontent.com/tidyblocks/tidyblocks/master/data/mtcars.csv'
   const result = `readCSV('${URL}')
     data`
-  tbLog('data_mtcars:', result)
   return result
 }
