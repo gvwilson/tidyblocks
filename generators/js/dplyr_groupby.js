ffcdff7f8bbb0733c1bd2a76d0de47a9427fddae
Maya <jaffe.maya@gmail.com>
2019-08-20 12:21:15 -0600

    mutate then groupby

--- generators/js/dplyr_groupby.js
+++ generators/js/dplyr_groupby.js
@@ -3,8 +3,6 @@
 //
 Blockly.JavaScript['dplyr_groupby'] = (block) => {
   const argColumns = Blockly.JavaScript.valueToCode(block, 'Columns', Blockly.JavaScript.ORDER_NONE)
-  return `.generateSeries({
-  Index: row => {return ${argColumns}}
-}).orderBy(column => column.Index)`
+  return `.generateSeries({ Index: row => ${argColumns}})`
     .replace(/&&/gi, '+')
 }
