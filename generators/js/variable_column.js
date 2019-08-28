8a41834d64834622005b87a9dc509086626c6120
Maya <jaffe.maya@gmail.com>
2019-08-28 11:34:25 -0600

    generator progress

--- generators/js/variable_column.js
+++ generators/js/variable_column.js
@@ -1,14 +1,16 @@
 Blockly.JavaScript['variable_column'] = (block) => {
-    var fieldText = [];
-    var index = 0;
-    while(true) {
-      var field = block.getFieldValue('TEXT' + index);
-      if (field) {
-        fieldText .push(field.block.getFieldValue('TEXT'));
-        index++;
-      } else {
-        break;
-      }
+  var fieldText = [];
+  var index = 0;
+  while(true) {
+    var field = block.getField('TEXT' + index);
+    if (field) {
+      fieldText.push(`"${field.getValue()}"`);
+      index++;
+    } else {
+      break;
     }
-    return code = fieldText.join(",")
   }
+  console.log(fieldText.join(","))
+  return [colValue(fieldText.join(",")), Blockly.JavaScript.ORDER_ATOMIC]
+};
+
