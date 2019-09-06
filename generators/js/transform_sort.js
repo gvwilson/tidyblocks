e0d21bb1e772934070a553aefdbec4c18e9a9585
Maya <jaffe.maya@gmail.com>
2019-09-06 11:47:12 -0600

    sort block, remove reverse block

--- generators/js/transform_sort.js
+++ generators/js/transform_sort.js
@@ -1,10 +1,14 @@
-//
-// Sort data by columns.
-//
 Blockly.JavaScript['transform_sort'] = (block) => {
-  const columns = block.getFieldValue('MULTIPLE_COLUMNS')
+
+
+const columns = block.getFieldValue('MULTIPLE_COLUMNS')
         .split(',')
         .map(c => `"${c.trim()}"`)
         .join(',')
-  return `.sort([${columns}])`
+
+if (block.getFieldValue('descending') == 'FALSE') {
+ 	 return `.sort([${columns}])`
+   } else {
+  	 return `.sort([${columns}]).reverse([${columns}])`
+   }
 }
