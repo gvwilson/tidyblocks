34835e0e46f6adf0c9f2ac82ea56d902e2218d61
Maya <jaffe.maya@gmail.com>
2019-09-13 18:07:53 -0600

    adding zoom

--- tidyblocks/gui.js
+++ tidyblocks/gui.js
@@ -134,6 +134,13 @@ const setUpBlockly = () => {
     {
       media: 'media/',
       toolbox: document.getElementById('toolbox'),
+      zoom:
+         {controls: true,
+          wheel: true,
+          startScale: 1.0,
+          maxScale: 3,
+          minScale: 0.3,
+          scaleSpeed: 1.2},
       horizontalLayout: false,
       scrollbars: false, 
       theme: Blockly.Themes.Tidy
