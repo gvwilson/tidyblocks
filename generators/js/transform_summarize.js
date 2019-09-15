49436a6baeedb822b15c3fa13657765d9792f6d2
Maya <jaffe.maya@gmail.com>
2019-09-14 19:16:38 -0600

    working on generator

--- generators/js/transform_summarize.js
+++ generators/js/transform_summarize.js
@@ -2,6 +2,6 @@
 // Summarize data:
 //
 Blockly.JavaScript['transform_summarize'] = (block) => {
-  const func = block.statementToCode('COLUMN_FUNC_PAIR')
+  const func = Blockly.JavaScript.statementToCode(block, 'COLUMN_FUNC_PAIR')
   return `.summarize(${func})`
 }
