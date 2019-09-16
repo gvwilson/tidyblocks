4e388f00485f6aef5c6bf152d0d62c85c18ddce5
Maya <jaffe.maya@gmail.com>
2019-09-16 07:27:09 -0600

    adding constraint and warning

--- blocks/transform_summarize_item.js
+++ blocks/transform_summarize_item.js
@@ -30,6 +30,50 @@ Blockly.defineBlocksWithJsonArray([
     nextStatement: null,
     style: "transform_blocks",
     tooltip: "",
-    helpUrl: ""
+    helpUrl: "",
+    extensions: "check_transform_summarize"
   } 
 ])
+
+Blockly.Constants.Loops.IN_SUMMARIZE_CHECK_MIXIN = {
+  /**
+   * List of block types allowed and thus do not need warnings.
+   */
+  SUMMARIZE: ['transform_summarize'],
+
+  /**
+   * Called whenever anything on the workspace changes.
+   * Add warning if block not inside Summarize
+   * @param {!Blockly.Events.Abstract} e Change event.
+   * @this Blockly.Block
+   */
+  onchange: function(/* e */) {
+    if (!this.workspace.isDragging || this.workspace.isDragging()) {
+      return;  // Don't change state at the start of a drag.
+    }
+    var legal = false;
+    // Is the block inside a Summarize
+    var block = this;
+    do {
+      if (this.SUMMARIZE.indexOf(block.type) != -1) {
+        legal = true;
+        break;
+      }
+      block = block.getSurroundParent();
+    } while (block);
+    if (legal) {
+      this.setWarningText(null);
+      if (!this.isInFlyout) {
+        this.setDisabled(false);
+      }
+    } else {
+      this.setWarningText('Warning: This block may only be used with the Summarize block');
+      if (!this.isInFlyout && !this.getInheritedDisabled()) {
+        this.setDisabled(true);
+      }
+    }
+  }
+};
+
+Blockly.Extensions.registerMixin('check_transform_summarize',
+    Blockly.Constants.Loops.IN_SUMMARIZE_CHECK_MIXIN);
