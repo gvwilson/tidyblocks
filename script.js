6e46c02995781c557972cc8b3bc9c85a891ed460
Greg Wilson <gvwilson@third-bit.com>
2020-03-23 14:42:32 -0400

    Style update

--- script.js
+++ script.js
@@ -1,70 +1,64 @@
-/* eslint
-   semi: ["error", "always"],
-   indent: [2, "tab"],
-   no-tabs: 0,
-   no-multiple-empty-lines: ["error", {"max": 2, "maxEOF": 1}],
-   one-var: ["error", "always"] */
-/* global REDIPS, briq */
+'use strict'
 
-/* enable strict mode */
-'use strict';
+// User interface initialization.
+let UI = null
+const setup = () => {
+  briq.Setup()
+  UI = briq.instance
+  Array.from(document.getElementsByClassName('buttonDefault'))
+    .forEach(b => b.click())
+  redips.init()
+}
 
-let UI, // eslint-disable-line no-unused-vars
-	redips = {}; // create redips container
+// REDIPS initialization.
+let redips = {
+  init: () => {
+    const rd = REDIPS.drag
 
-// define setup
-const setup = function () { // eslint-disable-line no-unused-vars
-	briq.Setup();
-	UI = briq.instance;
-	Array.from(document.getElementsByClassName('buttonDefault')).forEach(b => b.click());
-	// call REDIPS initialization
-	redips.init();
-};
+    // REDIPS settings
+    rd.init('redips-drag')
+    rd.dropMode = 'single'       // one item per cell
+    rd.trash.question = null     // don't confirm deletion
+    redips.colorTD = 'lightblue' // hover color for allowed TD
 
+    // set reference to "program table" and trash cell
+    redips.briqProgram = document.getElementById('briq-program')
+    redips.briqTrash = document.getElementById('briq-trash')
 
-// REDIPS initialization
-redips.init = function () {
-	let rd = REDIPS.drag;
-	// REDIPS settings
-	rd.init('redips-drag');
-	rd.dropMode = 'single'; // one item per cell
-	rd.trash.question = null; // don't confirm deletion
-	redips.colorTD = '#e7ab83'; // hover color for allowed TD
-	// set reference to "program table" and trash cell
-	redips.briqProgram = document.getElementById('briq-program');
-	redips.briqTrash = document.getElementById('briq-trash');
-	// event called before DIV element is dropped (td is target TD where DIV element is dropped)
-	rd.event.droppedBefore = function (td) {
-		// if dropped DIV element is not inside "program" table then return false (nothing will happen)
-		if (!redips.briqProgram.contains(td)) {
-			return false;
-		}
-	};
-	// handler clicked - set hover color
-	rd.event.clicked = function (td) {
-		redips.setHoverColor(td);
-	};
-	// handler changed - set hover color
-	rd.event.changed = function (td) {
-		redips.setHoverColor(td);
-	};
-};
+    // event called before DIV element is dropped (td is target TD where DIV element is dropped)
+    rd.event.droppedBefore = (td) => {
+      // if dropped DIV element is not inside "program" table then return false (nothing will happen)
+      return redips.briqProgram.contains(td)
+    }
 
+    // handler clicked - set hover color
+    rd.event.clicked = (td) => {
+      redips.setHoverColor(td)
+    }
 
-// method sets hover color (td is target cell)
-redips.setHoverColor = function (td) {
-	let rd = REDIPS.drag;
-	// set red color for trash TD
-	if (redips.briqTrash.contains(td)) {
-		rd.hover.colorTd = 'red';
-	}
-	// set hover color for allowed cells
-	else if (redips.briqProgram.contains(td)) {
-		rd.hover.colorTd = redips.colorTD;
-	}
-	// set hover color for forbiden cells (it'll be the same color - so user will not see difference)
-	else {
-		rd.hover.colorTd = 'lightgreen';
-	}
-};
+    // handler changed - set hover color
+    rd.event.changed = (td) => {
+      redips.setHoverColor(td)
+    }
+  },
+  
+  // method sets hover color (td is target cell)
+  setHoverColor: (td) => {
+    const rd = REDIPS.drag
 
+    // set red color for trash TD
+    if (redips.briqTrash.contains(td)) {
+      rd.hover.colorTd = 'red'
+    }
+
+    // set hover color for allowed cells
+    else if (redips.briqProgram.contains(td)) {
+      rd.hover.colorTd = redips.colorTD
+    }
+
+    // set hover color for forbiden cells (it'll be the same color - so user will not see difference)
+    else {
+      rd.hover.colorTd = 'lightgreen'
+    }
+  }
+}
