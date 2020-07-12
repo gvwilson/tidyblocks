d6b6c2ecf1e8f82eccfc9e5e20b01e0c1254f964
Greg Wilson <gvwilson@third-bit.com>
2020-07-12 17:32:26 -0400

    Encapsulating setup

--- jeff/index.js
+++ jeff/index.js
@@ -165,7 +165,23 @@ const theme = Blockly.Theme.defineTheme('jeff', {
 // Exports
 // ----------------------------------------------------------------------
 
+const setup = (divId, toolbox) => {
+  const settings = {
+    toolbox: toolbox,
+    zoom: {
+      controls: true,
+      wheel: true,
+      startScale: 1.0,
+      maxScale: 3,
+      minScale: 0.3,
+      scaleSpeed: 1.2
+    },
+    theme: theme
+  }
+  return Blockly.inject(divId, settings)
+}
+
 module.exports = {
   Blockly,
-  theme
+  setup
 }
