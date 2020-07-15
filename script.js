529259192ab09437bb2edbe468da2c1689b09048
Greg Wilson <gvwilson@third-bit.com>
2020-07-14 20:36:30 -0400

    Putting initialization in the HTML file

--- script.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-let UI = null
-const setup = () => {
-  tidyblocks.Setup(window.document)
-  UI = tidyblocks.instance
-  Array.from(document.getElementsByClassName('buttonDefault'))
-    .forEach(b => b.click())
-}
