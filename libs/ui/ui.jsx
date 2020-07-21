eed3fee1273aee51ae0590466e8a1e3d692f0e28
Justin Singh <justin.singh2@gmail.com>
2020-07-21 00:38:03 -0400

    Vega imports added.

--- libs/ui/ui.jsx
+++ libs/ui/ui.jsx
@@ -1,43 +1,71 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, {useState} from 'react';
 import SplitPane, {Pane} from 'react-split-pane';
 import ReactBlocklyComponent from 'react-blockly';
 import parseWorkspaceXml from 'react-blockly/src/BlocklyHelper.jsx';
-import ConfigFiles from './blocklyContent.jsx';
-// import DataGrid from 'react-data-grid';
-// import 'react-data-grid/dist/react-data-grid.css';
+import ConfigFiles from './blocklyConfig.jsx';
+import ReactDataGrid from 'react-data-grid';
+import colorData from '../../data/earthquakes.js'
 
+// Temporary test data for ReactDataGrid.
+const columns = [{ key: 'Time', name: 'Time', sortable: true, resizable: true},
+  { key: 'Latitude', name: 'Latitude', sortable: true, resizable: true },
+  { key: 'Longitude', name: 'Longitude', sortable: true, resizable: true },
+  { key: 'Depth_Km', name: 'Depth_Km', sortable: true, resizable: true },
+  { key: 'Magnitude', name: 'Magnitude', sortable: true, resizable: true }
+];
+const initialRows = colorData
 
-// Temporary import to test tables.
-// import colorData from '../../data/colors.js'
+// https://codesandbox.io/s/54pk3r46o4?from-embed=&file=/src/index.js
+// Create a table with sortable columns.
+const Grid = () => {
+  const [rows, setRows] = useState(initialRows);
+  const rowGetter = rowNumber => rows[rowNumber];
 
+  const sortRows = (initialRows, sortColumn, sortDirection) => rows => {
+    const comparer = (a, b) => {
+      if (sortDirection === "ASC") {
+        return a[sortColumn] > b[sortColumn] ? 1 : -1;
+      } else if (sortDirection === "DESC") {
+        return a[sortColumn] < b[sortColumn] ? 1 : -1;
+      }
+    };
+    return sortDirection === "NONE" ? initialRows : [...rows].sort(comparer);
+  };
+
+  return (<ReactDataGrid
+    columns={columns}
+    rowGetter={rowGetter}
+    rowsCount={rows.length}
+    minHeight={500}
+    onGridSort={(sortColumn, sortDirection) =>
+      setRows(sortRows(rows, sortColumn, sortDirection))
+    }
+    />)
+}
+
+// The main TidyBlocks App UI. Contains resizable panes for the Blockly section,
+// tabs for data display/plotting/logs.
 export class TidyBlocksApp extends React.Component{
     constructor(props){
       super(props);
       this.blocklyRef = React.createRef();
-
       this.state = {
         toolboxCategories: parseWorkspaceXml(this.props.toolbox)
-
       };
-
       this.paneResize = this.paneResize.bind(this);
-    }
+      this.updatePlot = this.updatePlot.bind(this);
 
-    workspaceDidChange(workspace){
-      alert("hi")
-      workspace.registerButtonCallback('myFirstButtonPressed', () => {
-        alert('button is pressed');
-      });
-      const newXml = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
-      document.getElementById('generated-xml').innerText = newXml;
-
-      const code = Blockly.JavaScript.workspaceToCode(workspace);
-      document.getElementById('code').value = code;
     }
 
     paneResize(){
       this.blocklyRef.current.resize()
+    }
+
+    updatePlot (spec) {
+      spec.width = FULL_PLOT_SIZE.width
+      spec.height = FULL_PLOT_SIZE.height
+      vegaEmbed('#plotOutput', spec, {})
     }
 
     render(){
@@ -58,6 +86,8 @@ export class TidyBlocksApp extends React.Component{
 
             <SplitPane split="horizontal" defaultSize={"50%"} minSize="200px">
               <Pane className="topRightPane" initialSize="50%">
+              <Grid
+                />
 
               </Pane>
               <Pane className="bottomRightPane" initialSize="50%">This Pane has a minimum size of 200px. It's the bottom pane.</Pane>
