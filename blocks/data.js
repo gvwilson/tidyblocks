a932320705b098232e10080ea18972c34328a2e1
Giovani Ferreira <gioferreira@gmail.com>
2020-08-15 19:49:08 -0300

    added pt_br key to message

--- blocks/data.js
+++ blocks/data.js
@@ -2,7 +2,9 @@
 
 const Blockly = require('blockly/blockly_compressed')
 
-const {Messages} = require('./helpers')
+const {
+  Messages
+} = require('./helpers')
 
 /**
  * Lookup table for message strings.
@@ -13,13 +15,15 @@ const MESSAGES = {
       en: 'Colors',
       es: 'Colores',
       ar: 'الألوان',
-      ko: '색깔'
+      ko: '색깔',
+      pt: 'Cores'
     },
     tooltip: {
       en: 'eleven colors',
       es: 'once colores',
       ar: 'احد عشر لون',
-      ko: '11개의 색'
+      ko: '11개의 색',
+      pt: 'onze cores'
     }
   },
   earthquakes: {
@@ -27,41 +31,47 @@ const MESSAGES = {
       en: 'Earthquakes',
       es: 'Terremotos',
       ar: 'الزلزال',
-      ko: '지진'
+      ko: '지진',
+      pt: 'Terremotos'
     },
     tooltip: {
-      en: 'earthquake data', 
+      en: 'earthquake data',
       es: 'datos de terremotos',
       ar: 'بيانات الزلزال',
-      ko: '지진 데이터'
+      ko: '지진 데이터',
+      pt: 'dados de terremotos'
     }
   },
   penguins: {
     message0: {
-      en: 'Penguins', 
+      en: 'Penguins',
       es: 'Pingüinos',
       ar: 'طيور البطريق',
-      ko: '펭귄'
+      ko: '펭귄',
+      pt: 'Penguins'
     },
     tooltip: {
       en: 'penguin data',
       es: 'datos de pingüinos',
       ar: 'بيانات طيور البطريق',
-      ko: '펭귄 데이터'
+      ko: '펭귄 데이터',
+      pt: 'dados de penguins'
     }
   },
   phish: {
     message0: {
-      en: 'Phish', 
+      en: 'Phish',
       es: 'Phish',
       ar: 'فرقه الفيش الموسيقيه',
-      ko: '피시'
+      ko: '피시',
+      pt: 'Phish'
     },
     tooltip: {
       en: 'Phish concert data',
       es: 'datos de conciertos Phish',
       ar: 'بيانات فرقه الفيش الموسيقيه',
-      ko: '피시 콘서트 데이터'
+      ko: '피시 콘서트 데이터',
+      pt: 'dados de shows do Phish'
     }
   },
   sequence: {
@@ -69,39 +79,45 @@ const MESSAGES = {
       en: 'Sequence %1 %2',
       es: 'Sequencia %1 %2',
       ar: 'المتسلسله %1 %2',
-      ko: '배열 %1 %2'
+      ko: '배열 %1 %2',
+      pt: 'Sequência %1 %2'
     },
     args0_text: {
-      en: 'name', 
+      en: 'name',
       es: 'nombre',
       ar: 'اﻹسم',
-      ko: '이름'
+      ko: '이름',
+      pt: 'nome'
     },
     tooltip: {
       en: 'Generate a sequence 1..N',
       es: 'Generar una sequencia 1..N',
       ar: 'إنشاء متسلسله ١..ن',
-      ko: '배열 실행 1..N'
+      ko: '배열 실행 1..N',
+      pt: 'Gerar uma sequencia 1..N'
     }
   },
   data_user: {
     message0: {
-      en: 'User data %1', 
+      en: 'User data %1',
       es: 'Datos de usuario %1',
       ar: 'بيانات المسته %1',
-      ko: '사용자 데이터 %1'
+      ko: '사용자 데이터 %1',
+      pt: 'Dados de usuario %1'
     },
     args0_text: {
       en: 'name',
       es: 'nombre',
       ar: 'الإسم',
-      ko: '이름'
+      ko: '이름',
+      pt: 'nome'
     },
     tooltip: {
-      en: 'use previously-loaded data', 
+      en: 'use previously-loaded data',
       es: 'usa datos previamente cargados',
       ar: 'إستخدام بيانات محمله مسبقا',
-      ko: '이전에 로드된 데이터 사용'
+      ko: '이전에 로드된 데이터 사용',
+      pt: 'use dados carregados previamente'
     }
   }
 }
@@ -157,8 +173,7 @@ const setup = (language) => {
     {
       type: 'data_sequence',
       message0: msg.get('sequence.message0'),
-      args0: [
-        {
+      args0: [{
           type: 'field_input',
           name: 'COLUMN',
           text: msg.get('sequence.args0_text')
@@ -179,13 +194,11 @@ const setup = (language) => {
     {
       type: 'data_user',
       message0: msg.get('data_user.message0'),
-      args0: [
-        {
-          type: 'field_input',
-          name: 'NAME',
-          text: msg.get('data_user.args0_text')
-        }
-      ],
+      args0: [{
+        type: 'field_input',
+        name: 'NAME',
+        text: msg.get('data_user.args0_text')
+      }],
       nextStatement: null,
       style: 'data_block',
       hat: 'cap',
