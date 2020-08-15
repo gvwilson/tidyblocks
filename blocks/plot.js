d9f17c2720dcdb245062a6b530bc0f08b8d66b19
Giovani Ferreira <gioferreira@gmail.com>
2020-08-15 20:01:34 -0300

    added pt_br key

--- blocks/plot.js
+++ blocks/plot.js
@@ -2,7 +2,9 @@
 
 const Blockly = require('blockly/blockly_compressed')
 
-const {Messages} = require('./helpers')
+const {
+  Messages
+} = require('./helpers')
 
 /**
  * Lookup table for message strings.
@@ -12,32 +14,37 @@ const MESSAGES = {
     en: 'name',
     es: 'nombre',
     ar: 'الإسم',
-    ko: '이름'
+    ko: '이름',
+    pt: 'nome'
   },
   x_axis: {
     en: 'X axis',
     es: 'eje X',
     ar: 'المحور الأفقي',
-    ko: 'X축'
+    ko: 'X축',
+    pt: 'eixo X'
   },
   y_axis: {
     en: 'Y axis',
     es: 'eje Y',
     ar: 'المحور الرأسي',
-    ko: 'Y축'
+    ko: 'Y축',
+    pt: 'eixo Y'
   },
   plot_bar: {
     message0: {
       en: 'Bar %1 %2 %3',
       es: 'Barras %1 %2 %3',
       ar: 'الأعمده %1 %2 %3',
-      ko: '막대 %1 %2 %3'
+      ko: '막대 %1 %2 %3',
+      pt: 'Barra %1 %2 %3'
     },
     tooltip: {
       en: 'create bar plot',
       es: 'crear grafico barras',
       ar: 'إنشاء رسم الأعمده البيانيه',
-      ko: '막대 그래프 만들기'
+      ko: '막대 그래프 만들기',
+      pt: 'criar gráfico de barras'
     }
   },
   plot_box: {
@@ -45,13 +52,15 @@ const MESSAGES = {
       en: 'Box %1 %2 %3',
       es: 'Cajas %1 %2 %3',
       ar: 'الصندوق %1 %2 %3',
-      ko: '박스 %1 %2 %3'
+      ko: '박스 %1 %2 %3',
+      pt: 'Caixa %1 %2 %3'
     },
     tooltip: {
       en: 'create box plot',
       es: 'crear grafico cajas',
       ar: 'إنشاء مخطط الصندوق ذو العارضتين',
-      ko: '박스 그래프 만들기'
+      ko: '박스 그래프 만들기',
+      pt: 'criar diagrama de caixas'
     }
   },
   plot_dot: {
@@ -59,13 +68,15 @@ const MESSAGES = {
       en: 'Dot %1 %2',
       es: 'Puntos %1 %2',
       ar: 'النقطه %1 %2',
-      ko: '도트 %1 %2'
+      ko: '도트 %1 %2',
+      pt: 'Ponto %1 %2'
     },
     tooltip: {
       en: 'create dot plot',
       es: 'crear grafico puntos',
       ar: 'إنشاء المخطط النقطي',
-      ko: '도트 그래프 만들기'
+      ko: '도트 그래프 만들기',
+      pt: 'criar gráfico de pontos'
     }
   },
   plot_histogram: {
@@ -73,19 +84,22 @@ const MESSAGES = {
       en: 'Histogram %1 %2 %3',
       es: 'Histograma %1 %2 %3',
       ar: 'المدرج التكراري %1 %2 %3',
-      ko: '히스토그램 %1 %2 %3'
+      ko: '히스토그램 %1 %2 %3',
+      pt: 'Histograma %1 %2 %3'
     },
     column: {
       en: 'column',
       es: 'columna',
       ar: 'العمود',
-      ko: '열'
+      ko: '열',
+      pt: 'coluna'
     },
     tooltip: {
       en: 'create histogram',
       es: 'crear histograma',
       ar: 'إنشاء المدرج التكراري',
-      ko: '히스토그램 만들기'
+      ko: '히스토그램 만들기',
+      pt: 'criar histograma'
     }
   },
   plot_scatter: {
@@ -93,13 +107,15 @@ const MESSAGES = {
       en: 'Scatter %1 %2 %3 Color %4 Add Line? %5',
       es: 'Dispersion %1 %2 %3 Color %4 Añadir linea? %5',
       ar: 'التشتت %1 %2 %3 اللون %4 إضافه خط؟ %5',
-      ko: '분산 %1 %2 %3 색깔 %4 선 추가? %5'
+      ko: '분산 %1 %2 %3 색깔 %4 선 추가? %5',
+      pt: 'Disperssão %1 %2 %3 Cor %4 Adicionar Linha? %5'
     },
     tooltip: {
       en: 'create scatter plot',
       en: 'crear grafico dispersion',
       ar: 'إنشاء مخطط الإنتشار',
-      ko: '분산 그래프 만들기'
+      ko: '분산 그래프 만들기',
+      pt: 'criar gráfico de dispersão'
     }
   }
 }
@@ -115,8 +131,7 @@ const setup = (language) => {
     {
       type: 'plot_bar',
       message0: msg.get('plot_bar.message0'),
-      args0: [
-        {
+      args0: [{
           type: 'field_input',
           name: 'NAME',
           text: msg.get('name')
@@ -145,8 +160,7 @@ const setup = (language) => {
     {
       type: 'plot_box',
       message0: msg.get('plot_box.message0'),
-      args0: [
-        {
+      args0: [{
           type: 'field_input',
           name: 'NAME',
           text: msg.get('name')
@@ -175,8 +189,7 @@ const setup = (language) => {
     {
       type: 'plot_dot',
       message0: msg.get('plot_dot.message0'),
-      args0: [
-        {
+      args0: [{
           type: 'field_input',
           name: 'NAME',
           text: msg.get('name')
@@ -200,8 +213,7 @@ const setup = (language) => {
     {
       type: 'plot_histogram',
       message0: msg.get('plot_histogram.message0'),
-      args0: [
-        {
+      args0: [{
           type: 'field_input',
           name: 'NAME',
           text: msg.get('name')
@@ -230,8 +242,7 @@ const setup = (language) => {
     {
       type: 'plot_scatter',
       message0: msg.get('plot_scatter.message0'),
-      args0: [
-        {
+      args0: [{
           type: 'field_input',
           name: 'NAME',
           text: msg.get('name')
