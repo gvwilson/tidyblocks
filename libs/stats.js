bcce93664d54e67f910cb0a763c87bac18dfa99e
Greg Wilson <gvwilson@third-bit.com>
2020-08-18 07:32:57 -0400

    Removing overall metric

--- libs/stats.js
+++ libs/stats.js
@@ -131,20 +131,6 @@ function calculateChange(left, right) {
 // ----------------------------------------------------------------------
 
 /**
- * Calculate the [silhouette metric](https://en.wikipedia.org/wiki/Silhouette_(clustering))
- * for a set of N-dimensional points arranged in groups. The metric is the largest
- * individual silhouette value for the data.
- * @param {Array<Array<number>>} points N-dimensional coordinates of points.
- * @param {Array<number>} labels Labels of points. This must be the same length as `points`,
- * and values must lie in [0..G-1], where G is the number of groups.
- * @returns {number} The silhouette metric for the groupings.
- */
-function silhouetteMetric(points, labels){
-    const values = silhouette(points, labels);
-    return Math.max(...values);
-}
-
-/**
  * Calculate the [silhouette values](https://en.wikipedia.org/wiki/Silhouette_(clustering))
  * for clustered data.
  * @param {Array<Array<number>>} points N-dimensional coordinates of points.
