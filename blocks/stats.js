29703de5f75c85bb3a94a5dfb25c0d809c33b66a
Boryana Koseva <boryana.k@gmail.com>
2020-08-31 18:57:17 -0500

    Update stats.js

--- blocks/stats.js
+++ blocks/stats.js
@@ -14,35 +14,40 @@ const MESSAGES = {
       es: 'T-test para una muestra',
       ar: 'إختبار (ت) لعينه واحده',
       ko: '독립표본 t-검정',
-      it: 'T-test di un campione'
+      it: 'T-test di un campione', 
+      el: 'Έλεγχος t-test ενός δείγματος'
     },
     message1: {
       en: 'name %1 column %2 mean \u03BC %3',
       es: 'nombre %1 columna %2 media \u03BC %3',
       ar: 'الإسم %1 العمود %2 الوسط الحسابي \u03BC %3',
       ko: '이름 %1 열 %2 평균 \u03BC %3', 
-      it: 'nome %1 colonna %2 media \u03BC %3' 
+      it: 'nome %1 colonna %2 media \u03BC %3', 
+      el: 'όνομα %1 στήλη %2 μέσος όρος \u03BC %3'
     },
     args1_name: {
       en: 'name',
       es: 'nombre',
       ar: 'الإسم',
       ko: '이름',
-      it: 'nome'
+      it: 'nome', 
+      el: 'όνομα'
     },
     args1_column: {
       en: 'column',
       es: 'columna',
       ar: 'العمود',
       ko: '열',
-      it: 'colonna'
+      it: 'colonna', 
+      el: 'στήλη'
     },
     tooltip: {
       en: 'perform one-sample two-sided t-test',
       es: 'hacer t-test para una muestra dos colas',
       ar: 'إختبار (ت) ذو الاتجاهين لعينه واحده',
       ko: '독립표본 양측 t-검정 수행',
-      it: 'eseguire il t-test su un solo campione su due lati'
+      it: 'eseguire il t-test su un solo campione su due lati', 
+      el: 'Έλεγχος t-test ενός δείγματος με συντελεστή δίπλευρης σημαντικότητας'
     }
   },
   stats_ttest_two: {
@@ -51,42 +56,50 @@ const MESSAGES = {
       es: 'T-test para dos muestras',
       ar: 'إختبار (ت) لعينتين',
       ko: '이표본 t-검정', 
-      it: 'T-test a due campioni'
+      it: 'T-test a due campioni', 
+      el: 'Μη συσχετισμένος έλεγχος t-test δύο δειγμάτων'
     },
     message1: {
       en: 'name %1 labels %2 values %3',
       es: 'nombre %1 etiquetas %2 valores %3',
       ar: 'الإسم %1 الفئه %2 القيم %3',
       ko: '이름 %1 라벨 %2 값 %3', 
-      it: 'nome %1 etichette %2 valori %3'
+      it: 'nome %1 etichette %2 valori %3', 
+      el: 'όνομα %1 ετικέτες %2 τιμές %3'
     },
     args1_name: {
       en: 'name',
       es: 'nombre',
       ar: 'الإسم',
       ko: '이름',
-      it: 'nome'
+      it: 'nome', 
+      el: 'όνομα'
     },
     args1_label: {
       en: 'label',
       es: 'etiqueta',
       ar: 'الفئه',
       ko: '라벨',
-      it: 'etichetta'
+      it: 'etichetta', 
+      el: 'ετικέτα'
+     
     },
     args1_column: {
       en: 'column',
       es: 'columna',
       ar: 'العمود',
       ko: '열',
-      it: 'colonna'
+      it: 'colonna', 
+      el: 'στήλη'
     },
     tooltip: {
       en: 'perform two-sample two-sided t-test',
       es: 'hacer t-test para dos muestras dos colas',
       ar: 'إختبار (ت) ذو الإتجاهين لعينتين',
       ko: '이표본 양측 t-검정 수행', 
-      it: 'eseguire un t-test a due campioni su due lati'
+      it: 'eseguire un t-test a due campioni su due lati', 
+      el: 'Έλεγχος t-test δύο δειγμάτων με συντελεστή δίπλευρης σημαντικότητας'
+      
     }
   }
 }
